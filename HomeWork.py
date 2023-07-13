fa86e526c27fb6a11b9b63b862aff0e44d8f5e7c
mishochubinidze1 <138606559+mishochubinidze1@users.noreply.github.com>
2023-07-13 14:06:30 +0400

    Add files via upload

---
+++ HomeWork.py
@@ -0,0 +1,19 @@
+"information about myself"
+name = "mishochubinidze"
+month = 'april'
+age = "18"
+
+
+"my hobby"
+movie = "vikings"
+director = "Ciaran Donnelly"
+
+book = "Harry Potter"
+author = "J. K. Rowling"
+
+travel = "spain"
+city = "barcelona"
+
+learn = "programming"
+academy = "(GOA Academy) Is The Best In The World"
+print(name + " - " + month + " - " + age + " - " + movie + " - " + director + " - " + book + " - " + author + " - " + travel + " - " + city + " - " + learn + " - " + academy )
